a82d52f486cac2d8ea94fe938d6cfbcd9bb93ad4
Mark Lybrand <mlybrand@gmail.com>
2015-06-25 15:19:27 -0700

    added calc class and tests to cover it

---
+++ WebApplication4/test/calc-test.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="jasmine.js"/>
+/// <reference path="../Scripts/calc.js"/>
+
+describe('Calculator - Addition', function () {
+
+    var calculator;
+
+    beforeAll(function() {
+        calculator = new Calculator();
+    });
+
+    it('should add two 2 and 2 and get 4', function() {
+        // Arrange
+        var x = 2,
+            y = 2,
+            expected = 4;
+
+        // Act
+        var actual = calculator.add(x, y);
+
+        // Assert
+        expect(actual).toEqual(expected);
+
+    });
+
+    it('should add 3 and 4 and get 7', function () {
+        // Arrange
+        var x = 3,
+            y = 4,
+            expected = 7;
+
+        // Act
+        var actual = calculator.add(x, y);
+
+        // Assert
+        expect(actual).toEqual(expected);
+    });
+});
